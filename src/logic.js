2c4d7c3a30a50e6567b53cdb8e8bbf3ece28d406
Evgeniy Evstafiev <angryaddict@gmail.com>
2021-01-15 14:06:08 +0300

    Brain-even done

---
+++ src/logic.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (num) => {
+  return num % 2 === 0;
+};
+export default function even() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const arr = [15, 6, 7];
+  for (const ar of arr) {
+    console.log(`Question: ${ar}`);
+    const answer = readlineSync.question(`Your answer: `);
+    const cAnswer = isEven(ar) ? 'yes' : 'no';
+    if (answer === cAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${cAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
